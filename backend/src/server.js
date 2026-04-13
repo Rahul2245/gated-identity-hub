4129e11b23814c3dde9cfcafe7ecb2df8a3c428d
Rahul2245 <rahulgs2024@gmail.com>
2026-04-13 18:41:18 +0530

    structured env, server.js and added db.js file config for posgres

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const logger =require("./middlewares/logger");
+const routes = require("./routes");
 const app = express();
 
-const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(logger);
+
+
+
+app.use("/api",routes);
+const PORT = process.env.PORT||5000;
+
+
+
 
 app.get('/',(req,res)=>{
     res.send("server running");
