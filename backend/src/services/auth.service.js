eeffae6eca22b3bcd9653006131b544829b1d67f
Rahul2245 <rahulgs2024@gmail.com>
2026-04-14 21:09:05 +0530

    auth service loginuser logic done

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,46 @@
+const {pool}=require("../config/db");
+const bcrypt = require("bcrypt");
+const { generateAccessToken } = require("../utils/jwt");
+const { generateRefreshToken } = require("../utils/token");
+
+const CreateOrGetUser = async (email) =>{
+    const result = await pool.query(
+        "SELECT * FROM users WHERE email=$1",[email]
+    );
+      if (result.rows.length > 0) return result.rows[0];
+
+    const newUser = await pool.query(
+        "INSERT INTO users (email) VALUES ($1) RETURNING *",
+        [email]
+    );
+
+    return newUser.rows[0];
+};
+
+const loginUser = async (email) => {
+    const user = await createOrGetUser(email);
+
+    const accessToken = generateAccessToken({
+        id: user.id,
+        role: user.role,
+    });
+
+    const refreshToken = generateRefreshToken();
+
+    const hashedToken = await bcrypt.hash(refreshToken, 10);
+
+    const expiresAt = new Date();
+    expiresAt.setHours(expiresAt.getHours() + 24);
+
+    await pool.query(
+        `INSERT INTO refresh_tokens (user_id, hashed_token, expires_at)
+         VALUES ($1, $2, $3)`,
+        [user.id, hashedToken, expiresAt]
+    );
+
+    return { accessToken, refreshToken };
+};
+
+module.exports = {
+    loginUser,
+};
