880cb59533538eec8e92fd7f789b791b60178e68
Rahul2245 <rahulgs2024@gmail.com>
2026-04-20 17:23:26 +0530

    made chnages while testing refresh tokens

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const generateAccessToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: "15m",
-    });
+    });  
 };
 
 const verifyAccessToken = (token) => {
